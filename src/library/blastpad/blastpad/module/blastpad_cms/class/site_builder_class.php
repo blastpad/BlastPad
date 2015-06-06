217db355e97a0db338c55a52cadbde068f3ae7f9
Honey Silvas <honeysilvas@gmail.com>
2015-06-05 20:14:49 -0400

    Removed unused code.
    Removed unused code.

--- src/library/blastpad/blastpad/module/blastpad_cms/class/site_builder_class.php
+++ src/library/blastpad/blastpad/module/blastpad_cms/class/site_builder_class.php
@@ -15,8 +15,6 @@ class site_builder {
 		}
 		
 		return $site_array;
-	}
-	
+	}	
 }
-
 ?>
--- src/library/blastpad/blastpad/module/blastpad_cms/model/site.php
+++ src/library/blastpad/blastpad/module/blastpad_cms/model/site.php
@@ -1,24 +1,11 @@
 <?php
 namespace blastpad;
-require_once( dirname(__FILE__) . "../../../calliope_database/class/model_class.php" );
 
 class site extends model {
 	
 	private $site;
 	
 	function set_default(){
-		require_once ( dirname(__FILE__) . "../../../calliope_database/class/model_class.php" );
-		
-	/*	global $database;
-		// Get site info.
-		$row = $database -> fetch( "select site_id, site_name, site_tagline, site_theme, site_slug
-			from site
-			where site_is_default = 1
-			LIMIT 1
-			" ); 
-		$this -> site = $row[ 0 ];
-*/
-
 		global $default_site_theme;
 		global $site_name;
 		global $site_tagline;
@@ -31,7 +18,5 @@ class site extends model {
 		
 		return $this -> site;
 	}
-	
 }
-
 ?>
